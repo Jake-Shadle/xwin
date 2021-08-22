3476b246625612c90ea3fedd0608f0da8da595e6
Jake Shadle <jake.shadle@embark-studios.com>
2021-08-22 08:25:41 +0200

    Minor cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![doc = include_str!("../README.md")]
 // BEGIN - Embark standard lints v0.4
 // do not change or add/remove here, but one can add exceptions after this section
 // for more info see: <https://github.com/EmbarkStudios/rust-ecosystem/issues/59>
