b9e2a7098cc9d3f622a47c795780793a483b8087
Jaime Geiger <geiger_jaime@bah.com>
2026-01-22 17:05:58 -0700

    Add rustls-platform-verifier support

--- src/main.rs
+++ src/main.rs
@@ -291,15 +291,21 @@ fn main() -> Result<(), Error> {
             let proxy = ureq::Proxy::new(&proxy).context("failed to parse https proxy address")?;
             builder = builder.proxy(Some(proxy));
         }
+        // Mutated iff platform-verifier or native-tls features are enabled
+        #[allow(unused_mut)]
+        let mut tls_config = ureq::tls::TlsConfig::builder();
+
+        #[cfg(feature = "platform-verifier")]
+        {
+            tls_config = tls_config.root_certs(ureq::tls::RootCerts::PlatformVerifier);
+        }
 
         #[cfg(feature = "native-tls")]
         {
-            builder = builder.tls_config(
-                ureq::tls::TlsConfig::builder()
-                    .provider(ureq::tls::TlsProvider::NativeTls)
-                    .build(),
-            );
+            tls_config = tls_config.provider(ureq::tls::TlsProvider::NativeTls);
         }
+
+        builder = builder.tls_config(tls_config.build());
 
         builder.build().new_agent()
     };
